5991c4305fbd0b4b2d76f2dac0339f4fb7058f55
Matt Mower <mowerm@gmail.com>
2016-04-17 23:10:14 -0500

    Rename wifi module
    We don't need to deviate from the standard wlan module name. Requires
    associated change to kernel.
    
    Change-Id: I4bef538483a313e5ad5c68fa31b239d1f62e403b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,7 +66,7 @@ TARGET_USES_WCNSS_CTRL := true
 TARGET_USES_QCOM_WCNSS_QMI := true
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
-WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/prima_wlan/parameters/fwpath"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/wlan/parameters/fwpath"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Filesystem
